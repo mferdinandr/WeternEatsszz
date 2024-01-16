dfe71d19a714b4272cae74fc1e8856c09d73cd8a
Ferdinand Ramdhani <ferdinram13@gmail.com>
2024-01-16 21:49:25 +0700

    feat - headline card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import HeadlineCards from './components/HeadlineCards';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+      <HeadlineCards></HeadlineCards>
     </>
   );
 }
